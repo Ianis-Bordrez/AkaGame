9663ac65d83561e4ee387a8b6efc966384bcd6af
laurent.marechal@ynov.com <laurent.marechal@ynov.com>
2020-02-04 23:52:47 +0100

    add

--- uilogin.py
+++ uilogin.py
@@ -28,26 +28,26 @@ class WindowLogin(QMainWindow):
         self.lineedit1.setPlaceholderText("Please Enter Your name")
         self.lineedit1.setGeometry(540, 200, 200, 30)
         self.lineedit1.setStyleSheet(
-            "background-color : transparent; color : black")
+            "background-color : transparent; color : black; border : 1px solid black; border-radius: 5px;")
 
         self.lineedit2 = QLineEdit(self)
         self.lineedit2.setPlaceholderText("Please Enter Your password")
         self.lineedit2.setGeometry(540, 250, 200, 30)
         self.lineedit2.setStyleSheet(
-            "background-color : transparent; color : black")
+            "background-color : transparent; color : black; border : 1px solid black; border-radius: 5px;")
 
         self.button = QPushButton("Login", self)
         self.button.resize(150, 60)
         self.button.move(565, 300)
         self.button.setStyleSheet(
-            "background-color: transparent; font-size: 20px; border : 1px solid black")
+            "background-color: transparent; font-size: 20px; border : 2px solid black; border-radius : 20px")
         self.button.clicked.connect(self.btn_login)
 
         self.button2 = QPushButton("Create Account", self)
         self.button2.resize(150, 60)
         self.button2.move(565, 370)
         self.button2.setStyleSheet(
-            "background-color: transparent; font-size: 20px; border : 1px solid black")
+            "background-color: transparent; font-size: 20px; border : 2px solid black; border-radius : 20px")
         self.button2.clicked.connect(self.btn_create_account)
 
         self.show()
