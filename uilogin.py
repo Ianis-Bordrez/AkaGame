052c10869f73746a1fb219443866fc0e8401d734
Ianis BORDREZ <ianis.bordrez@gmail.com>
2020-02-05 11:23:13 +0100

    Add connection check

--- uilogin.py
+++ uilogin.py
@@ -24,50 +24,43 @@ class WindowLogin(QMainWindow):
         self.label.setPixmap(QPixmap("img/imgbckg.jpg"))
         self.label.setGeometry(0, 0, 1280, 800)
 
-        self.lineedit1 = QLineEdit(self)
-        self.lineedit1.setPlaceholderText("Please Enter Your name")
-        self.lineedit1.setGeometry(540, 200, 200, 30)
+        self.username = QLineEdit(self)
+        self.username.setPlaceholderText("Please Enter Your name")
+        self.username.setGeometry(540, 200, 200, 30)
 
-        self.lineedit2 = QLineEdit(self)
-        self.lineedit2.setPlaceholderText("Please Enter Your password")
-        self.lineedit2.setGeometry(540, 250, 200, 30)
+        self.password = QLineEdit(self)
+        self.password.setPlaceholderText("Please Enter Your password")
+        self.password.setGeometry(540, 250, 200, 30)
 
-        self.button = QPushButton("Login", self)
-        self.button.resize(100, 60)
-        self.button.move(590, 300)
-        self.button.clicked.connect(self.btn_login)
+        self.btn_login = QPushButton("Login", self)
+        self.btn_login.resize(100, 60)
+        self.btn_login.move(590, 300)
+        self.btn_login.clicked.connect(self.login)
 
-        self.button = QPushButton("Create Account", self)
-        self.button.resize(100, 60)
-        self.button.move(590, 370)
-        self.button.clicked.connect(self.btn_create_account)
+        self.btn_create_account = QPushButton("Create Account", self)
+        self.btn_create_account.resize(100, 60)
+        self.btn_create_account.move(590, 370)
+        self.btn_create_account.clicked.connect(self.create_account)
 
         self.show()
 
-    def btn_login(self):
-        username = self.username.text()
-        password = self.password.text()
-        if len(username) < 4 or len(password) < 4:
-            return False
+    def login(self):
+        if self.check_login(self.username.text(), self.password.text()):
+            pass
 
+    def create_account(self):
+        self.close()
+        self.next = uicreateaccount.WindowCreateAccount()
+
+    def check_login(self, username, password):
         myDataBase = Database(constinfo.mysql_config)
         myDataBase.connect()
 
         query = f"SELECT login,password FROM account WHERE login='{username}'"
         result = myDataBase.get(query)
-        print(result)
         if result is None:
-            QMessageBox.about(self, "Connection",
-                              "Wrong username or password")
-        else:
-            if self.check_login(self, username, password):
-                pass
+            QMessageBox.about(self, "Connection", "Wrong username or password")
+            return False
 
-    def btn_create_account(self):
-        self.close()
-        self.next = uicreateaccount.WindowCreateAccount()
-
-    def check_login(self, username, password):
-
-        QMessageBox.about(self, "Create Account", "Email not valid")
-        return False
+        QMessageBox.about(self, "Connection", "Successful connection")
+        return True
