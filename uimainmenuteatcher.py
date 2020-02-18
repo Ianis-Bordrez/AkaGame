ba738537deef802482136355278a0692009e6eca
laurent.marechal@ynov.com <laurent.marechal@ynov.com>
2020-02-18 14:02:25 +0100

    add button link

--- uimainmenuteatcher.py
+++ uimainmenuteatcher.py
@@ -9,4 +9,19 @@ class WindowTeatcher(ui.Window):
         ui.Window.__init__(self, "AkaGame | Teatcher main window")
         self.init_window()
         self.init_background("img/bckg_create_char.jpg")
+        self.init_button()
         self.show()
+
+    def init_button(self):
+        self.btn_create_quiz = QPushButton("Create a quiz", self)
+        self.btn_create_quiz.resize(150, 60)
+        self.btn_create_quiz.move(525, 420)
+        self.btn_create_quiz.setStyleSheet("QPushButton { font-size: 20px; border : 2px solid black; border-radius : 20px }"
+                                           "QPushButton:hover { background-color: rgba(50, 50, 50, 0.5); font-size: 20px; border : 2px solid black; border-radius : 20px }"
+                                           "QPushButton:pressed { background-color: rgba(250, 250, 250, 0.5); font-size: 20px; border : 2px solid black; border-radius : 20px }")
+        self.btn_create_quiz.clicked.connect(
+            self.create_quiz)
+
+    def create_quiz(self):
+        self.close()
+        self.next = uicreatequiz.WindowCreateQuiz()
